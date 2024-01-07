b805e5d6b51f37c2253f828700409420c52a9cf6
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-01-07 11:25:11 -0500

    ran some anovas on yield and made a boxplot

--- PA_yield.R
+++ PA_yield.R
@@ -33,3 +33,35 @@ test_mean %>%
   group_by(year) %>% 
   distinct(plot, .keep_all = TRUE) %>% 
   print(n = Inf)
+
+# all data cleaning ####
+yield
+
+yield$plot <- gsub('-[0-9.]','', yield$plot) # remove - and all numbers following
+
+
+yield_clean <- yield%>% 
+  group_by(year, plot) %>% 
+  mutate(lb_pass_mean = mean(lb_pass_moisture), 
+         lb_ac_mean = mean(lb_ac),
+         bu_ac_mean = mean(bu_ac)) %>% 
+  select(-lb_pass_moisture, -lb_ac, -bu_ac) %>% 
+  mutate(year = as.factor(year)) %>% 
+  distinct(plot, .keep_all = TRUE) %>% 
+  print(n = Inf)
+as_tibble(yield_clean)
+
+# Anova ####
+anova_one <- aov(bu_ac_mean ~ trt + year, yield_clean)
+summary(anova_one)
+plot(residuals(anova_one))
+hist(residuals(anova_one))
+TukeyHSD(anova_one)
+
+anova_two <- aov(bu_ac_mean ~ year, yield_clean)
+summary(anova_two)
+hist(residuals(anova_two))
+TukeyHSD(anova_two)
+
+ggplot(yield_clean, aes(x = year, y = bu_ac_mean, fill = trt))+
+  geom_boxplot()
