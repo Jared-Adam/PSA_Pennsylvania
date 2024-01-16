0b5751816bf347693811284d346685eb4c38d9f5
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-01-16 08:18:26 -0500

    made some ggplots and cleaned data

--- PA_slugs.R
+++ PA_slugs.R
@@ -18,14 +18,57 @@ test_slug %>%
   mutate(date = as.Date(date, "%m/%d/%Y"),
          year = format(date, '%Y')) %>% 
   rename(precip = "7day_precip") %>% 
-  select(-date, -precip) %>%
-  group_by(year, month, plot_id, treatment, block) %>% 
+  select(-date, -precip) %>% 
+  group_by(season, year, month, plot_id, treatment, block) %>% 
   summarise(total_slug =  sum(slug_count)) %>% 
   print(n = Inf)
 ?summarise
-# this is not working correctly... 
+# it is by month, so the numbers may seem larges
 
+# whole data set 
 
-slugs %>% 
-  filter(month == 'june') %>% 
+slug_clean <- slugs %>% 
+  select(-location, -shingle_id, -time, -temp, -row) %>% 
+  mutate(date = as.Date(date, "%m/%d/%Y"),
+         year = format(date, '%Y')) %>% 
+  rename(precip = "7day_precip") %>% 
+  select(-date, -precip) %>% 
+  mutate(year = as.factor(year), 
+       treatment = as.factor(treatment))%>% 
+  group_by(season, year, month, plot_id, treatment, block) %>% 
+  summarise(total_slug =  sum(slug_count))%>% 
   print(n = Inf)
+
+#subset by season
+
+fall_slugs <- subset(slug_clean, season == "fall")
+spring_slugs <- subset(slug_clean, season == "spring")
+
+
+#over dispersion check: yes
+mean(fall_slugs$total_slug, na.rm = TRUE)
+var(fall_slugs$total_slug, na.rm = TRUE)
+mean(spring_slugs$total_slug, na.rm = TRUE)
+var(spring_slugs$total_slug, na.rm = TRUE)
+
+# data vis: good ggplots
+
+ggplot(spring_slugs, aes(x = treatment, y = total_slug, fill = year))+
+  geom_boxplot()+
+  facet_wrap(~year)+
+  ggtitle("Total Spring Slugs by Treatment")+
+  scale_x_discrete(labels=c("Check", "Brown", "Green", "Gr-Br"))+
+  ylab("Total slug counts")+
+  xlab("")+
+  theme(axis.text.x = element_text(size=12),
+        axis.text.y = element_text(size = 12))
+
+ggplot(fall_slugs, aes(x = treatment, y = total_slug, fill = year))+
+  geom_boxplot()+
+  facet_wrap(~year)+
+  ggtitle("Total Spring Slugs by Treatment")+
+  scale_x_discrete(labels=c("Check", "Brown", "Green", "Gr-Br"))+
+  ylab("Total slug counts")+
+  xlab("")+
+  theme(axis.text.x = element_text(size=12),
+        axis.text.y = element_text(size = 12))
