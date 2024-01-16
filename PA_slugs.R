5b0462c3a0fd69f72e5256e7ee7d661cb328cdba
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-01-16 10:04:59 -0500

    taking a break from slugs

--- PA_slugs.R
+++ PA_slugs.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(emmeans)
 library(lme4)
+library(MASS)
 # data ####
 slugs <- PSA_PA_slugs
 
@@ -14,17 +15,27 @@ slugs
   
 colnames(slugs)
 test_slug <- slugs[1:200,]
+test_slug$month <- gsub(" ", "", test_slug$month)
 test_slug %>% 
-  select(-location, -shingle_id, -time, -temp, -row) %>% 
+  dplyr::select(-location, -shingle_id, -time, -temp, -row) %>% 
   mutate(date = as.Date(date, "%m/%d/%Y"),
          year = format(date, '%Y')) %>% 
   rename(precip = "7day_precip") %>% 
-  select(-date, -precip) %>% 
+  dplyr::select(-date) %>% 
   group_by(season, year, month, plot_id, treatment, block) %>% 
-  summarise(total_slug =  sum(slug_count)) %>% 
+  summarise(total_slug =  sum(slug_count)) %>%  
   print(n = Inf)
-?summarise
-# it is by month, so the numbers may seem larges
+
+# # precip 
+# test_slug %>% 
+#   dplyr::select(-location, -shingle_id, -time, -temp, -row) %>% 
+#   mutate(date = as.Date(date, "%m/%d/%Y"),
+#          year = format(date, '%Y')) %>% 
+#   rename(precip = "7day_precip") %>% 
+#   group_by(season, year, month, block) %>% 
+#   summarise(total_precip = sum(precip))
+
+
 
 # whole data set 
 
@@ -68,6 +79,31 @@ hist(residuals(spring_model))
 spring_emm <- emmeans(spring_model, pairwise ~ treatment, type = "response")
 pairs(spring_emm) # will print the contrasts
 plot(spring_emm$emmeans)
+
+# glm.nb from MASS
+glm_test <- glm(total_slug ~ treatment, family = poisson, data = spring_slugs)
+summary(glm_test)
+hist(residuals(glm_test))
+
+glm.nb_test <-glm.nb(total_slug ~ treatment + year, data = spring_slugs)
+summary(glm.nb_test)
+hist(residuals(glm.nb_test))
+shapiro.test(residuals(glm.nb_test))
+
+
+# 1/16/2023
+# I am unsure which model to run. the random effects model is out because the random effects has little weight. 
+# i then tried a glm and glm.nb, but the residuals are ass. 
+# I refuse to transform, so what is next? 
+
+
+
+
+
+
+
+
+
 
 ggplot(spring_slugs, aes(x = treatment, y = total_slug, fill = year))+
   geom_boxplot()+
