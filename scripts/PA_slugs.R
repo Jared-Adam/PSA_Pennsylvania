31eaeacfc48968824a0ef8cdc16ab95cf4383511
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-01-23 12:31:23 -0500

    I deleted the slug script changes somehow, here are my recreations

--- scripts/PA_slugs.R
+++ scripts/PA_slugs.R
@@ -4,7 +4,9 @@
 library(tidyverse)
 library(emmeans)
 library(lme4)
+library(lmtest)
 library(MASS)
+library(nlme)
 # data ####
 slugs <- PSA_PA_slugs
 
@@ -58,15 +60,6 @@ spring_slugs <- subset(slug_clean, season == "spring")
 
 # models ####
 
-# no more of this: 
-  # use the new overdispersion code 
-# #over dispersion check: yes
-# mean(fall_slugs$total_slug, na.rm = TRUE)
-# var(fall_slugs$total_slug, na.rm = TRUE)
-# mean(spring_slugs$total_slug, na.rm = TRUE)
-# var(spring_slugs$total_slug, na.rm = TRUE)
-
-
 # look at overdispersion: variance > mean?
 dispersion_stats <- slug_clean %>% 
   group_by(treatment) %>%
@@ -92,14 +85,14 @@ test_spring_model <- glmer.nb(total_slug ~ treatment+
                                 (1|block/plot_id)+ (1|month), data = spring_slugs)
 summary(test_spring_model)
 
-# the random of effects of this model are extremely low, this means I likely do not need to include them
-spring_model <- glmer.nb(total_slug ~ treatment +
-                           (1|block) + (1|month), 
-                         data = spring_slugs)
-summary(spring_model)
-spring_emm <- emmeans(spring_model, pairwise ~ treatment, type = "response")
-pairs(spring_emm) # will print the contrasts
-plot(spring_emm$emmeans)
+# # the random of effects of this model are extremely low, this means I likely do not need to include them
+# spring_model <- glmer.nb(total_slug ~ treatment +
+#                            (1|block) + (1|month), 
+#                          data = spring_slugs)
+# summary(spring_model)
+# spring_emm <- emmeans(spring_model, pairwise ~ treatment, type = "response")
+# pairs(spring_emm) # will print the contrasts
+# plot(spring_emm$emmeans)
 
 # glm.nb from MASS
 glm_test <- glm(total_slug ~ treatment, family = poisson, data = spring_slugs)
@@ -109,28 +102,48 @@ glm.nb_test <-glm.nb(total_slug ~ treatment + year, data = spring_slugs)
 summary(glm.nb_test)
 
 
-# 1/16/2023
-# I am unsure which model to run. the random effects model is out because the random effects has little weight. 
-# i then tried a glm and glm.nb, but the residuals are ass. 
-# I refuse to transform, so what is next? 
-
-
-
 # let's see which is better, poisson or nb? 
 # run one of each where the only difference is the family 
-library(lmtest)
-library(MASS)
-poisson_model <- glmer(total_score ~ treatment + 
-                         (1|block) + (1|date), 
-                       data = rodale_glmer, 
+poisson_model <- glmer(total_slug ~ treatment + 
+                         (1|year/block), 
+                       data = slug_clean, 
                        family = poisson)
 
-nb_model_trt <- glmer.nb(total_score ~ treatment + 
-                           (1|block) + (1|date), 
-                         data = rodale_glmer) 
+nb_model_trt <- glmer.nb(total_slug ~ treatment + 
+                           (1|year/block), 
+                         data = slug_clean) 
 
 lrtest(poisson_model,nb_model_trt)
 # the negative binomial has the higher likelihood score, so we will use that
+
+
+# with random of block, plot, and season: the models are singular 
+m1 <- glmer.nb(total_slug ~ treatment +
+                 (1|year), data = slug_clean) 
+
+
+summary(m1)
+check_singularity(m1)
+r2_nakagawa(m1)
+binned_residuals(m1)
+m1_r <- binned_residuals(m1)
+plot(m1_r)
+
+
+# jw model attempt 
+
+lme_df <- na.omit(slug_clean)
+jw_m <- nlme::lme(total_slug ~ treatment, random = ~ 1|year, 
+             correlation = nlme::corCAR1(form=~1|year), 
+             data = lme_df)
+
+summary(jw_m)
+check_singularity(jw_m)
+r2_nakagawa(jw_m)
+binned_residuals(jw_m)
+jw_m_r <- binned_residuals(jw_m)
+plot(jw_m_r)
+
 
 
 
