8f73b11dde8a7027fc342f1ee3e670ccca5bd563
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-02-05 13:03:48 -0500

    figured out how to pivot wider sum of characters

--- crop/corn/scripts/PA_abundance.R
+++ crop/corn/scripts/PA_abundance.R
@@ -6,5 +6,24 @@
 library(tidyverse)
 
 # data ####
-
 pf <- corn_pf
+
+# wrangling test ####
+# need to pivot wider to get my taxa names as columns with counts 
+pf <- pf %>% 
+  arrange(date, plot)
+test <- pf[1:95, ]
+
+# pivot <- test %>% 
+#   select(-split, -life_stage, -sp, -genus) %>% 
+#   group_by(date, plot, family) %>% 
+#   summarise(n_dist_families = n_distinct(family)) %>% 
+#   pivot_wider(names_from = family, 
+#               values_from = n_dist_families)
+
+pivot<-test %>% 
+  select(-split, -life_stage, -sp, -genus) %>% 
+  group_by(date, plot) %>% 
+  pivot_wider(names_from = family, 
+              values_from = family,
+              values_fn = list(family = length))
