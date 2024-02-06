5056f7fc90dfd5b4b9c6f667f0a048a2aeb971c9
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-02-06 12:07:31 -0500

    got ordiplot3d to work. but DAMN this took too long

--- crop/beans/script/bean_abundance.r
+++ crop/beans/script/bean_abundance.r
@@ -126,6 +126,8 @@ bperm_3
 #
 
 # NMDS PF ####
+
+library(rgl) # nope
 #
 ##
 ###
@@ -175,3 +177,133 @@ bord_3$stress
 ###
 ##
 #
+# test plot from stack overflow 
+
+data(dune,dune.env)
+SITE_ID <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t")
+dune.env$SITE_ID <- SITE_ID
+?cca
+ord <- cca(dune ~ A1 + Moisture, dune.env)
+ordiplot3d(ord)
+pl4 <- with(dune.env, ordiplot3d(ord, col = Management, pch=16,angle=50))
+
+with(dune.env, ordihull(pl4, dune.env$Management, draw = "poly", col = 1:4,label=T,
+                        alpha = 50))
+
+sp <- scores(pl4, choices=1:3, display="sites", scaling="symmetric")
+spp <- as.data.frame(cbind(dune.env$SITE_ID,sp))
+with(dune.env, ordilabel(pl4,labels=spp$V1,col="black", fill=NA, border=NA, pos = 2))
+
+# my turn 
+# going to chull trt into the df with my nmds and species names 
+# using bord_3 for nmds
+# using df bpf_2023
+bpf_2022 <- filter(bpf_clean, year == 2022)
+
+bean_family_names_22 <- bpf_2022[6:24]
+
+bord_22_3 <- metaMDS(bean_family_names_22, k = 3)
+
+
+scrs <-scores(bord_22_3, display = 'sites')
+scrs <- cbind(as.data.frame(scrs), trt = bpf_2022$trt)
+
+fsc <- as.data.frame(scores(bord_22_3, 'species'))
+fsc$species <- rownames(fsc)
+
+
+check <- scrs[scrs$trt == '1',][chull(scrs[scrs$trt == '1', c('NMDS1', "NMDS2", "NMDS3")]),]
+brown <- scrs[scrs$trt == '2',][chull(scrs[scrs$trt == '2', c('NMDS1', "NMDS2", "NMDS3")]),]
+green <- scrs[scrs$trt == '3',][chull(scrs[scrs$trt == '3', c('NMDS1', "NMDS2", "NMDS3")]),]
+grbr <- scrs[scrs$trt == '4',][chull(scrs[scrs$trt == '4', c('NMDS1', "NMDS2", "NMDS3")]),]
+
+hull.data <- rbind(check, brown, green, grbr)
+as_tibble(hull.data) %>% 
+  print(n= Inf)
+
+# I now have a df with 3 dimensions of nmds values and associates treatments with species scrs above
+##
+# plot window must be the appropriate size to fit the points and the polygons 
+
+ordiplot3d(bord_22_3)
+tpl <- with(bpf_2022, ordiplot3d(bord_22_3, col = trt, pch = 16, angle = 50))
+with(bpf_2022, ordihull(tpl, groups = bpf_2022$trt, draw = "poly", 
+                        col = 1:4, 
+                        label = F,
+                        border = F,
+                        alpha = 50))
+text(tpl$xyz.convert(fsc), rownames(fsc))
+legend(x = 'top', legend = levels(bpf_2022$trt), col = 1:4, pch = 16)
+
+##
+
+
+
+
+# tout <- ordiplot3d(bord_3, col = 'red', ax.col = 'black', pch = 18)
+# any(!is.finite(bpf_2023$trt))
+# is.na(bpf_2023$trt)
+# unique(bpf_2023$trt)
+# 
+# 
+# ordiplot3d(bord_3)
+# tpl <- with(bpf_2023, ordiplot3d(bord_3, col = trt, pch = 16, angle = 50))
+# with(bpf_2023, ordihull(tpl, groups = bpf_2023$trt, draw = "poly", 
+#                         col = 1:4, 
+#                         label = T, 
+#                         alpha = 50))
+# 
+# tsp <- scores(bord_3, choices = 1:3, display = 'species')
+# tspp <- as.data.frame(cbind(bpf_2023$trt, tsp))
+# with(bpf_2023, ordilabel(tpl, labels))
+
+
+##
+#this gets species names but no trts
+fsc
+
+bang <- ordiplot3d(bord_3, display = 'species')
+text(bang$xyz.convert(fsc), rownames(fsc))
+with(bang, ordiplot3d(bord_3, group = hull.data$trt, pch = 16, angle = 50))
+for (i in seq (1,4)) ordihull(bang, groups = hull.data$trt, show.group = i, col = i, 
+                              lty = 'dotted')
+
+bang <- ordiplot3d(bord_3, display = 'species')
+text(tpl$xyz.convert(fsc), rownames(fsc))
+for(i in unique(hull.data$trt)) ordihull(bang, groups = hull.data$trt, show.group = i, 
+                                        col = i,
+                                        draw = 'polygon')
+
+
+
+plot <- ordiplot3d(bord_3, display = 'sites')
+for(i in unique(hull.data$trt)) ordihull(plot, groups = hull.data$trt,
+                                         draw = 'poly', 
+                                         col = i)
+
+##
+
+# ellipse <- ordiellipse(bang, groups = hull.data$trt, kind = 'ehull', dar = 'polygon')
+
+
+
+
+##
+
+
+tout <- ordiplot3d(bord_3, col = 'red', ax.col = 'black', pch = 18)
+ordiplot3d(bord_3)
+tpl <- with(bpf_2023, ordiplot3d(bord_3, col = trt, pch = 16, angle = 50))
+with(bpf_2023, ordihull(tpl, bpf_2023$trt, draw = "poly", 
+                        col = 1:4, 
+                        label = T, 
+                        alpha = 50))
+
+tsp <- scores(bord_3, choices = 1:3, display = 'species')
+tspp <- as.data.frame(cbind(bpf_2023$trt, tsp))
+with(bpf_2023, ordilabel(tpl, labels))
+
+
+
+
+
