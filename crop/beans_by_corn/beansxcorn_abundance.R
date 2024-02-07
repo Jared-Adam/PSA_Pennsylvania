84bd90319a183cee1c1f5ffd7ecce2d3f55c4354
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-02-07 08:58:17 -0500

    data in and wrangled. functional  groups next

--- crop/beans_by_corn/beansxcorn_abundance.R
+++ crop/beans_by_corn/beansxcorn_abundance.R
@@ -1,3 +1,109 @@
 # Jared Adam 
 # how are they populations changing among year and crop
   # in the same field?
+# started on 2/7/2024
+# using 2022 corn and 2023 beans
+
+# packages ####
+library(tidyverse)
+library(vegan)
+library(vegan3d)
+
+# data ####
+beans <- bean_pf
+corn <- corn_pf
+
+
+# wrangling ####
+# pivot, add year, subset by year, cbind together, functional groups? 
+#
+##
+###
+# beans 
+colnames(beans)
+b_wide <- beans %>% 
+  select(-split, -life_stage, -sp, -genus) %>% 
+  group_by(date, plot) %>% 
+  pivot_wider(names_from = family, 
+              values_from = family,
+              values_fn = list(family = length)) %>% 
+  print(n = Inf)
+
+colnames(b_wide)
+b_wide <- b_wide  %>% 
+  replace(is.na(.),0) %>% 
+  mutate(Lin = Liniphiide + Lyniphiidae + Linyphiidae) %>% 
+  select(-Liniphiide, -Lyniphiidae, -Linyphiidae, -na) %>% 
+  mutate(date = as.Date(date, "%m/%d/%Y"), 
+         year = format(date, "%Y")) %>% 
+  relocate(year) %>% 
+  mutate(year = as.factor(year)) %>% 
+  mutate_at(3:5, as.factor) 
+colnames(b_wide)
+
+b_clean <- b_wide %>% 
+  mutate(trt = as.factor(case_when(plot %in% c(101,203,304,401,503) ~ 1,
+                         plot %in% c(103,204,302,403,501) ~ 2,
+                         plot %in% c(102,201,303,402,502) ~ 3, 
+                         plot %in% c(104,202,301,404,504) ~ 4))) %>% 
+  na.omit() %>%  
+  select(-crop) %>% 
+  mutate(crop = 'beans',
+         crop = as.factor(crop)) %>% 
+  relocate(year, date, crop) %>% 
+  print(n = Inf)
+colnames(b_clean)
+
+b_23 <- b_clean %>% 
+  filter(year == 2023) %>% 
+  print(n = Inf)
+
+###
+##
+#
+#
+##
+###
+# corn 
+colnames(corn)
+c_wide <- corn %>% 
+  select(-split, -life_stage, -sp, -genus) %>% 
+  group_by(date, plot) %>% 
+  pivot_wider(names_from = family, 
+              values_from = family,
+              values_fn = list(family = length)) %>% 
+  print(n = Inf)
+
+colnames(c_wide)
+c_wide <- c_wide  %>% 
+  replace(is.na(.),0) %>% 
+  mutate(Lyn = Liniphiidae + Lyniphiidae + Linyphiidae, 
+         Staph= Staphylinidae + Staphylinidaa) %>% 
+  select(-Liniphiidae, -Lyniphiidae, -Linyphiidae, -Staphylinidae, -Staphylinidaa, -na) %>% 
+  mutate(date = as.Date(date, "%m/%d/%Y"), 
+         year = format(date, "%Y")) %>% 
+  relocate(year) %>% 
+  mutate(year = as.factor(year)) %>% 
+  mutate_at(3:5, as.factor) 
+colnames(c_wide)
+
+c_clean <- c_wide %>% 
+  mutate(trt = as.factor(case_when(plot %in% c(101,203,304,401,503) ~ 1,
+                         plot %in% c(103,204,302,403,501) ~ 2,
+                         plot %in% c(102,201,303,402,502) ~ 3, 
+                         plot %in% c(104,202,301,404,504) ~ 4))) %>% 
+  na.omit() %>%
+  print(n = Inf)
+colnames(c_clean)
+
+c_22 <- c_clean %>% 
+  filter(year == 2022) %>% 
+  print(n = Inf)
+
+###
+##
+#
+
+# functional groups? ####
+
+
