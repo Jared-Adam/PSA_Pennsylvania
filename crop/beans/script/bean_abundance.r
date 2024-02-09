8c09918c6ad5d0e27e77e5e4675b65ad70f7fb47
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-02-09 11:27:44 -0500

    saving files as htmls

--- crop/beans/script/bean_abundance.r
+++ crop/beans/script/bean_abundance.r
@@ -18,7 +18,7 @@ bpf <- bpf %>%
 bpf_test <- bpf[1:95, ]
 
 bpf_pivot <- bpf_test %>% 
-  select(-split, -life_stage, -sp, -genus) %>% 
+  dplyr::select(-split, -life_stage, -sp, -genus) %>% 
   group_by(date, plot) %>% 
   pivot_wider(names_from = family, 
               values_from = family,
@@ -26,7 +26,7 @@ bpf_pivot <- bpf_test %>%
 # wrangling ####
 # whole data set 
 bpf_wide <- bpf %>% 
-  select(-split, -life_stage, -sp, -genus) %>% 
+  dplyr::select(-split, -life_stage, -sp, -genus) %>% 
   group_by(date, plot) %>% 
   pivot_wider(names_from = family, 
               values_from = family,
@@ -37,7 +37,7 @@ colnames(bpf_wide)
 bpf_wide <- bpf_wide  %>% 
   replace(is.na(.),0) %>% 
   mutate(Lin = Liniphiide + Lyniphiidae + Linyphiidae) %>% 
-  select(-Liniphiide, -Lyniphiidae, -Linyphiidae, -na) %>% 
+  dplyr::select(-Liniphiide, -Lyniphiidae, -Linyphiidae, -na) %>% 
   mutate(date = as.Date(date, "%m/%d/%Y"), 
          year = format(date, "%Y")) %>% 
   relocate(year) %>% 
@@ -51,7 +51,7 @@ bpf_clean <- bpf_wide %>%
                          plot %in% c(102,201,303,402,502) ~ 3, 
                          plot %in% c(104,202,301,404,504) ~ 4))) %>% 
   na.omit() %>%  
-  select(-crop) %>% 
+  dplyr::select(-crop) %>% 
   mutate(crop = 'beans',
          crop = as.factor(crop)) %>% 
   relocate(year, date, crop) %>% 
@@ -95,30 +95,38 @@ bord_22_3 <- metaMDS(bean_family_names_22, k = 3)
 bord_22_3$stress
 
 # plot
+b_22_scrs <- scores(bord_22_3, display = "sites")
+b_22_trt <- cbind(as.data.frame(b_22_scrs), trt = bpf_2022$trt)
 
 b_22_fsc <- as.data.frame(scores(bord_22_3, "species"))
 b_22_fsc$species <- rownames(b_22_fsc)
 
+plot_22 <- plot_ly(b_22_trt, x = ~NMDS1, y = ~NMDS2, z = ~NMDS3, color = ~trt)
+plot_22 <- plot_22 %>% 
+  add_markers()
+plot_22
 
-ordiplot3d(bord_22_3)
-tpl <- with(bpf_2022, ordiplot3d(bord_22_3, col = timing, pch = 16, angle = 50))
-with(bpf_2022, ordihull(tpl, groups = bpf_2022$timing, draw = "poly", 
-                        col = 1:3, 
-                        label = F,
-                        border = F,
-                        alpha = 50))
-text(tpl$xyz.convert(b_22_fsc), rownames(b_22_fsc), cex = 1.2)
-legend(x = 'right', legend = c("2022-05-28", "2022-07-01", "2022-08-18"), col = 1:3, pch = 16, cex = 2)
-legend(0, -0.5, "Stress: 0.118642",
-       xjust = 0.5,
-       yjust = 3, 
-       x.intersp = -0.5,
-       y.intersp = 0.1, 
-       adj = c(0,0.5), 
-       cex = 1.5)
-title(main ="NMDS of 2022 population distributions by timing",
-      cex.main = 2)
 
+
+# ordiplot3d(bord_22_3)
+# tpl <- with(bpf_2022, ordiplot3d(bord_22_3, col = timing, pch = 16, angle = 50))
+# with(bpf_2022, ordihull(tpl, groups = bpf_2022$timing, draw = "poly", 
+#                         col = 1:3, 
+#                         label = F,
+#                         border = F,
+#                         alpha = 50))
+# text(tpl$xyz.convert(b_22_fsc), rownames(b_22_fsc), cex = 1.2)
+# legend(x = 'right', legend = c("2022-05-28", "2022-07-01", "2022-08-18"), col = 1:3, pch = 16, cex = 2)
+# legend(0, -0.5, "Stress: 0.118642",
+#        xjust = 0.5,
+#        yjust = 3, 
+#        x.intersp = -0.5,
+#        y.intersp = 0.1, 
+#        adj = c(0,0.5), 
+#        cex = 1.5)
+# title(main ="NMDS of 2022 population distributions by timing",
+#       cex.main = 2)
+# 
 
 ###
 ##
@@ -157,28 +165,55 @@ bord_23_3 <- metaMDS(bean_family_names_23, k = 3)
 bord_23_3$stress
 
 
-# plot 
+# plot
+
+b_23_scrs <- scores(bord_23_3, display = "sites")
+b_23_trt <- cbind(as.data.frame(b_23_scrs), trt = bpf_2023$trt)
+
+
 b_23_fsc <- as.data.frame(scores(bord_23_3, 'species'))
 b_23_fsc$species <- rownames(b_23_fsc)
 
-ordiplot3d(bord_23_3)
-tpl_2 <- with(bpf_2023, ordiplot3d(bord_23_3, col = timing, pch = 16, angle = 50))
-with(bpf_2023, ordihull(tpl_2, groups = bpf_2023$timing, draw = "poly", 
-                        col = 1:3, 
-                        label = F,
-                        border = F,
-                        alpha = 50))
-text(tpl_2$xyz.convert(b_23_fsc), rownames(b_23_fsc), cex = 1.2)
-legend(x = -3, y = -0.5, legend = c("2023-06-26", "2023-07-28"), col = 1:3, pch = 16, cex = 2)
-legend(0.5, -0.5, "Stress: 0.1732693",
-       xjust = 0.5,
-       yjust = 3, 
-       x.intersp = -0.5,
-       y.intersp = 0.1, 
-       adj = c(0,0.5), 
-       cex = 1.5)
-title(main ="NMDS of 2023 population distributions by timing",
-      cex.main = 2)
+b_23_date_scrs <- scores(bord_23_3, display = "sites")
+b_23_date <- cbind(as.data.frame(b_23_date_scrs), date = bpf_2023$date)
+
+testy<- cbind(b_23_trt, b_23_date) %>% 
+  distinct(NMDS1, NMDS2, NMDS3, trt, date) %>% 
+  mutate(date = case_when(date == "2023-06-26" ~ "1",
+                          date == "2023-07-28" ~ "2"),
+         date = as.factor(date))
+
+
+
+# ?inner_join
+# test_join <- full_join(b_23_fsc, b_23_trt)
+
+plot_23 <- plot_ly(b_23_trt, x = ~NMDS1, y = ~NMDS2, z = ~NMDS3, color = ~trt)
+plot_23 <- plot_23 %>% 
+  add_markers()
+plot_23
+
+
+plot_date <- plot_ly(testy, x = ~NMDS1, y = ~NMDS2, z = ~NMDS3, color = ~date)
+plot_date <- plot_date %>% 
+  add_markers()
+
+plot_date
+
+?htmlwidgets::saveWidget
+# checking size of the html
+widget_file_size <- function(plot_date) {
+  d <- tempdir()
+  withr::with_dir(d, htmlwidgets::saveWidget(plot_date, "index.html"))
+  f <- file.path(d, "index.html")
+  mb <- round(file.info(f)$size / 1e6, 3)
+  message("File is: ", mb," MB")
+}
+widget_file_size(plot_date)
+
+# saving widget to folder PSA_Pennsylvania
+htmlwidgets::saveWidget(plot_date, "plant_date.html", selfcontained = F, libdir = "lib")
+
 
 
 ###
@@ -218,87 +253,22 @@ bord_3$stress
 
 # plot 
 
+b_scrs <- scores(bord_3, display = "sites")
+b_trt <- cbind(as.data.frame(b_scrs), trt = bpf_year$trt)
+
+
+b_fsc <- as.data.frame(scores(bord_3, 'species'))
+b_fsc$species <- rownames(b_fsc)
+
+
+
+
 b_all_fsc <- as.data.frame(scores(bord_3, 'species'))
 b_all_fsc$species <- rownames(b_all_fsc)
-
-ordiplot3d(bord_3)
-tpl_3 <- with(bpf_clean, ordiplot3d(bord_3, col = year, pch = 16, angle = 50))
-with(bpf_clean, ordihull(tpl_3, groups = bpf_clean$year, draw = "poly", 
-                        col = 1:3, 
-                        label = F,
-                        border = F,
-                        alpha = 50))
-#text(tpl$xyz.convert(b_all_fsc), rownames(b_all_fsc), cex = 1.2)
-legend(x = -3, y = -0.5, legend = c("2022", "2023"), col = 1:3, pch = 16, cex = 2)
-legend(0.5, -0.5, "Stress: 0.1471073",
-       xjust = 0.5,
-       yjust = 3, 
-       x.intersp = -0.5,
-       y.intersp = 0.1, 
-       adj = c(0,0.5), 
-       cex = 1.5)
-title(main ="NMDS of 2022 and 2023 population distributions by year",
-      cex.main = 2)
-
-# for after corn is done 
-legend(0.5, -0.5, "Timing p-value: 0.0001***",
-       xjust = 0.5,
-       yjust = 4.5, 
-       x.intersp = -0.5,
-       y.intersp = 0.1, 
-       adj = c(0,0.5), 
-       cex = 1.5)
-
 
 
 ###
 ##
 #
-
-# practice/ test plots ####
-# test plot from stack overflow 
-# 
-# data(dune,dune.env)
-# SITE_ID <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t")
-# dune.env$SITE_ID <- SITE_ID
-# ?cca
-# ord <- cca(dune ~ A1 + Moisture, dune.env)
-# ordiplot3d(ord)
-# pl4 <- with(dune.env, ordiplot3d(ord, col = Management, pch=16,angle=50))
-# 
-# with(dune.env, ordihull(pl4, dune.env$Management, draw = "poly", col = 1:4,label=T,
-#                         alpha = 50))
-# 
-# 
-# sp <- scores(pl4, choices=1:3, display="sites", scaling="symmetric")
-# spp <- as.data.frame(cbind(dune.env$SITE_ID,sp))
-# with(dune.env, ordilabel(pl4,labels=spp$V1,col="black", fill=NA, border=NA, pos = 2))
-
-
-##
-#this gets species names but no trts
-fsc
-
-bang <- ordiplot3d(bord_3, display = 'species')
-text(bang$xyz.convert(fsc), rownames(fsc))
-with(bang, ordiplot3d(bord_3, group = hull.data$trt, pch = 16, angle = 50))
-for (i in seq (1,4)) ordihull(bang, groups = hull.data$trt, show.group = i, col = i, 
-                              lty = 'dotted')
-
-bang <- ordiplot3d(bord_3, display = 'species')
-text(tpl$xyz.convert(fsc), rownames(fsc))
-for(i in unique(hull.data$trt)) ordihull(bang, groups = hull.data$trt, show.group = i, 
-                                        col = i,
-                                        draw = 'polygon')
-
-
-
-plot <- ordiplot3d(bord_3, display = 'sites')
-for(i in unique(hull.data$trt)) ordihull(plot, groups = hull.data$trt,
-                                         draw = 'poly', 
-                                         col = i)
-
-
-
 # anova ####
 bpf_clean
