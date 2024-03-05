2739587a831896bdd0e2210886c425aa34150ab5
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-03-05 10:20:22 -0500

    Damage inc and type
    Revised incidence and the slug type figure

--- crop/corn/scripts/corn_damage.R
+++ crop/corn/scripts/corn_damage.R
@@ -194,21 +194,25 @@ ggplot(slug_em, aes(color = treatment))+
   facet_wrap(~growth_stage)+
   scale_color_manual(values = c("#E7298A", "#D95F02", "#1B9E77", "#7570B3"))+
   scale_x_discrete(limits = c("1", "2", "4", "3"),
-                   labels=c("Check", "Brown", "GrBr", "Green"))+ 
+                   labels=c("No CC", "14-21 DPP", "3-7 DPP", "1-3 DAP"))+ 
   labs(
-    title = "Slug damage",
+    title = "Corn: Slug damage",
     subtitle = "Years: 2021-2023",
     x = "Treatment",
     y = "Damage probability"
   )+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 18), 
-        axis.text.y = element_text(size = 18),legend.position = "none",
-        strip.text = element_text(size = 16),
-        axis.title.x = element_text(size = 20),
-        axis.title.y = element_text(size = 20),
-        plot.title = element_text(size = 20),
-        plot.subtitle = element_text(s = 16), 
-        panel.grid.major = element_blank(),
+  theme(legend.position = 'none',
+        axis.title = element_text(size = 20),
+        plot.subtitle = element_text(size = 18),
+        plot.title = element_text(size = 24),
+        # axis.line = element_line(size = 1.25),
+        # axis.ticks = element_line(size = 1.25),
+        # axis.ticks.length = unit(.25, "cm"),
+        axis.text.x = element_text(size = 18),
+        axis.text.y = element_text(size = 18),
+        strip.text.x = element_text(size = 20), 
+        panel.grid.major.y = element_line(color = "darkgrey"),
+        panel.grid.major.x = element_blank(),
         panel.grid.minor = element_blank())
 
 bcw_em
--- crop/corn/scripts/corn_inc.R
+++ crop/corn/scripts/corn_inc.R
@@ -89,59 +89,7 @@ pwpm(m1_em)
 
 # plot ####
 
-# ggplot(plot_emm, aes(color = treatment))+
-#   geom_point(aes(x = treatment, y = prob), size = 5,
-#              position = position_dodge(width = .75))+
-#   facet_wrap(~growth)
-#   geom_errorbar(aes(x = treatment,ymin = prob - SE, ymax = prob + SE),
-#                 color = "black", alpha = 1, width = 0, linewidth = 2)+
-#   geom_errorbar(aes(x = treatment,ymin = asymp.LCL, ymax = asymp.UCL), 
-#                 alpha = .6, width = 0, linewidth = 1.5)+
-#   scale_color_manual(values = c("#E7298A", "#D95F02", "#1B9E77", "#7570B3"))+
-#   scale_x_discrete(labels=c("Check", "Brown", "Green", "Gr-Br"))+ 
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12), 
-#         axis.text.y = element_text(size = 12))+
-#   annotate("text", x = 3.25, y = 0.9, label = "p = 0.000522 ***", size = 6)+
-#   labs(
-#     title = "Corn: Damage incidence by treatment",
-#     subtitle = "Years: 2021-2023",
-#     y = "Proportion damaged (damaged / total)",
-#     x = "Treatment"
-#   )+
-#   theme(legend.position = 'none',
-#         axis.text.x = element_text(size=18, angle = 45, hjust = 1),
-#         axis.text.y = element_text(size = 18),
-#         strip.text = element_text(size = 16),
-#         axis.title = element_text(size = 20),
-#         plot.title = element_text(size = 20),
-#         plot.subtitle = element_text(s = 16), 
-#         panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank())
 
-ggplot(dam_plot, aes(color = treatment))+
-  geom_point(aes(x = treatment, y = mean), size = 5,
-             position = position_dodge(width = .75))+
-  geom_errorbar(aes(x = treatment,ymin = mean - se, ymax = mean + se),
-                color = "black", alpha = 1, width = 0.2, linewidth = 2)+
-  scale_color_manual(values = c("#E7298A", "#D95F02", "#1B9E77", "#7570B3"))+
-  scale_x_discrete(limits = c("1", "2", "4", "3"),
-                   labels=c("Check", "Brown", "GrBr", "Green"))+ 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 18), 
-        axis.text.y = element_text(size = 18))+
-  annotate("text", x = 4, y = 0.82, label = "b", size = 6)+
-  labs(
-    title = "Corn: Damage incidence by treatment",
-    subtitle = "Years: 2021-2023",
-    y = "Mean proportion damaged (damaged / total)",
-    x = "Treatment"
-  )+
-  theme(legend.position = 'none',
-        strip.text = element_text(size = 16),
-        axis.title = element_text(size = 20),
-        plot.title = element_text(size = 20),
-        plot.subtitle = element_text(s = 16), 
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
 
 
 ggplot(dam_plot, aes(color = treatment))+
@@ -152,20 +100,26 @@ ggplot(dam_plot, aes(color = treatment))+
                 color = "black", alpha = 1, width = 0.2, linewidth = 2)+
   scale_color_manual(values = c("#E7298A", "#D95F02", "#1B9E77", "#7570B3"))+
   scale_x_discrete(limits = c("1", "2", "4", "3"),
-                   labels=c("Check", "Brown", "GrBr", "Green"))+ 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 18), 
+                   labels=c("No CC", "14-21 DPP", "3-7 DPP", "1-3 DAP"))+ 
+  theme(axis.text.x = element_text(size = 18), 
         axis.text.y = element_text(size = 18))+
   labs(
-    title = "Corn: Damage",
+    title = "Corn: Damage Incidence",
     subtitle = "Years: 2021-2023",
     y = "Mean proportion damaged (damaged / total)",
     x = "Treatment"
   )+
   theme(legend.position = 'none',
-        strip.text = element_text(size = 16),
         axis.title = element_text(size = 20),
-        plot.title = element_text(size = 20),
-        plot.subtitle = element_text(s = 16), 
-        panel.grid.major = element_blank(),
+        plot.subtitle = element_text(size = 18),
+        plot.title = element_text(size = 24),
+        # axis.line = element_line(size = 1.25),
+        # axis.ticks = element_line(size = 1.25),
+        # axis.ticks.length = unit(.25, "cm"),
+        axis.text.x = element_text(size = 18),
+        axis.text.y = element_text(size = 18),
+        strip.text.x = element_text(size = 20), 
+        panel.grid.major.y = element_line(color = "darkgrey"),
+        panel.grid.major.x = element_blank(),
         panel.grid.minor = element_blank())
 
