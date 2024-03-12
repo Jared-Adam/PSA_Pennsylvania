4eaacfdb708e5bf251d71c9284e614b55472b001
Jared Adam <79530043+Jared-Adam-PSU@users.noreply.github.com>
2024-03-12 09:43:03 -0400

    Beans slugs
    models and plots revised

--- crop/beans/script/beans_slugs.r
+++ crop/beans/script/beans_slugs.r
@@ -31,7 +31,7 @@ slugs <- slugs_beans_all %>%
   dplyr::select(-location, -shingle_id, -time, -temp, -row) %>% 
   mutate(date = as.Date(date, "%m/%d/%Y"),
          year = format(date, '%Y'))  %>% 
-  dplyr::select(-date, -precip) %>% 
+  dplyr::select(-date) %>% 
   mutate(year = as.factor(year), 
        treatment = as.factor(treatment))%>% 
   rename(season = seaon) %>% 
@@ -47,6 +47,46 @@ slugs <- slugs %>%
 unique(slugs$treatment)
 unique(slugs$season)
 
+# getting precip 
+slug_precip <- slugs_beans_all %>% 
+  mutate(slug_count = as.numeric(slug_count)) %>% 
+  rename(precip = '7_day_precip_in') %>% 
+  mutate(temp = as.numeric(temp)) %>% 
+  mutate(treatment = case_when(plot %in% c(101,203,304,401,503) ~ 1,
+                               plot %in% c(103,204,302,403,501) ~ 2,
+                               plot %in% c(102,201,303,402,502) ~ 3, 
+                               plot %in% c(104,202,301,404,504) ~ 4)) %>% 
+  mutate(block = case_when(plot %in% c(101,102,103,104) ~ 1,
+                           plot %in% c(201,202,203,204) ~ 2,
+                           plot %in% c(301,302,303,304) ~ 3,
+                           plot %in% c(401,402,403,404) ~ 4,
+                           plot %in% c(501,502,503,504) ~ 5)) %>% 
+  mutate(block = as.factor(block)) %>%
+  dplyr::select(-location, -shingle_id, -time, -temp, -row) %>% 
+  mutate(date = as.Date(date, "%m/%d/%Y"),
+         year = format(date, '%Y'))  %>% 
+  dplyr::select(-date) %>% 
+  mutate(year = as.factor(year), 
+         treatment = as.factor(treatment))%>% 
+  rename(season = seaon) %>% 
+  mutate(season = case_when(season == "fall" ~ "Fall",
+                            season == "spring" ~ "Spring"))%>% 
+  group_by(year, season, month, plot) %>% 
+  summarise(sum_pre = sum(precip)) %>% 
+  print(n = Inf)
+slug_precip <- slug_precip[1:160,]
+slug_precip <- slug_precip %>% 
+  replace(is.na(.),0) %>%
+  ungroup() %>% 
+  dplyr::select(sum_pre) %>% 
+  print(n = Inf)
+  
+final_slug <- cbind(slug_precip, slugs) 
+
+final_slug <- final_slug %>% 
+  mutate_at(vars(2:7), as.factor) %>% 
+  relocate(season, year, month, plot, treatment, block)
+  as_tibble()
 
 #subset by season
 
@@ -84,57 +124,39 @@ poisson_model <- glmer(total_slug ~ treatment +
 
 nb_model_trt <- glmer.nb(total_slug ~ treatment + 
                            (1|year/block), 
-                         data = slugs) 
+                         data = final_slug) 
 
 lrtest(poisson_model,nb_model_trt)
 # the negative binomial has the higher likelihood score, so we will use that
 
 #actual model: 
-unique(slugs$season)
-is.factor(slugs$season)
-slugs <- slugs %>% 
+# with precip, the model was overfit and not getting an r2 value 
+unique(final_slug$season)
+is.factor(final_slug$season)
+final_slug <- final_slug %>% 
   mutate(season = as.factor(season))
-model_1 <- glmer.nb(total_slug ~ treatment+ (1|year/block), 
-                    data = slugs)
-summary(model_1)
-r2_nakagawa(model_1)
-binned_residuals(model_1)
-br_1 <- binned_residuals(model_1)
+
+m0 <- glmer.nb(total_slug ~ 
+                      (1|year/season/block), 
+                    data = final_slug)
+
+
+m1 <- glmer.nb(total_slug ~ treatment + (1|year/season/block), 
+                    data = final_slug)
+
+anova(m0, m1)
+
+summary(m1)
+r2_nakagawa(m1)
+# Conditional R2: 0.734
+# Marginal R2: 0.010
+binned_residuals(m1)
+br_1 <- binned_residuals(m1)
 plot(br_1)
-s_emm <- emmeans(model_1, ~treatment, type = "response")
+s_emm <- emmeans(m1, ~treatment, type = "response")
 pairs(s_emm)
+cld(s_emm, Letters = letters)
 
-# confirming trt differences 
-m3 <- kruskal.test(total_slug ~ treatment, data  = bs_22)
-m5 <- kruskal.test(total_slug ~ season, data  = bs_22)
-
-m4 <- kruskal.test(total_slug ~ treatment, data = bs_23)
-m6 <- kruskal.test(total_slug ~ season, data = bs_23)
-
-
-# glm for trt*szn no random 
-model_2 <- MASS::glm.nb(total_slug ~ treatment + season, data = slugs)
-summary(model_2)
-hist(residuals(model_2))
-
-# spring sig diff than fall and trt 4 sig diff from 1
-total_slug_22 <- dplyr::filter(slugs, year == "2022")
-model_3 <- MASS::glm.nb(total_slug ~ treatment + season,
-                        data = total_slug_22)
-summary(model_3)
-hist(residuals(model_3))
-
-# sping sig from fall 
-total_slug_23 <- filter(slugs, year == "2023")
-model_4 <- MASS::glm.nb(total_slug ~ treatment + season, 
-                       data = total_slug_23)
-summary(model_4)
-hist(residuals(model_4))
-
-
-
-
-# significance between fall and spring, but not year or treatment
 
 # plots for slug populations  ####
 # add sig values in ppt: confusing with two factor facets
@@ -160,12 +182,61 @@ ggplot(slugs, aes(x = as.character(treatment), y = total_slug, fill = treatment)
         panel.grid.major.x = element_blank(),
         panel.grid.minor = element_blank())
 
+slug_plot <- final_slug %>% 
+  group_by(treatment) %>% 
+  summarise(mean = mean(total_slug), 
+            sd = sd(total_slug), 
+            n = n(), 
+            se = sd/sqrt(n))
 
+ggplot(slug_plot, aes(x = treatment, y = mean, fill = treatment))+
+  geom_bar(stat = "identity", position = "dodge", alpha = 0.7)+
+  geom_errorbar(aes(x = treatment,ymin = mean - se, ymax = mean + se),
+                color = "black", alpha = 1, width = 0.2, linewidth = 1)+
+  scale_fill_manual(values = c("#E7298A", "#D95F02", "#1B9E77", "#7570B3"))+
+  scale_x_discrete(limits = c("1", "2", "4", "3"),
+                   labels=c("No CC", "14-21 DPP", "3-7 DPP", "1-3 DAP"))+
+  labs( x = 'Treatment',
+        y = 'Total Slug Counts', 
+        title = "Soybean: Average Slug Counts / Trap x Treatment",
+        subtitle = " Years: 2022-2023",
+        caption = "DPP: Days pre plant
+DAP : Days after plant")+
+  theme(legend.position = "none",
+        axis.text.x = element_text(size=26),
+        axis.text.y = element_text(size = 26),
+        axis.title = element_text(size = 32),
+        plot.title = element_text(size = 28),
+        plot.subtitle = element_text(size = 24), 
+        panel.grid.major.y = element_line(color = "darkgrey"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.caption = element_text(hjust = 0, size = 20, color = "grey25"))
 
+# pub plots ####
 
-
-
-
+ggplot(slug_plot, aes(x = treatment, y = mean))+
+  geom_bar(stat = "identity", position = "dodge", alpha = 0.7)+
+  geom_errorbar(aes(x = treatment,ymin = mean - se, ymax = mean + se),
+                color = "black", alpha = 1, width = 0.2, linewidth = 1)+
+  scale_x_discrete(limits = c("1", "2", "4", "3"),
+                   labels=c("No CC", "14-21 DPP", "3-7 DPP", "1-3 DAP"))+
+  labs( x = 'Treatment',
+        y = 'Total Slug Counts', 
+        title = "Soybean: Average Slug Counts / Trap x Treatment",
+        subtitle = " Years: 2022-2023",
+        caption = "DPP: Days pre plant
+DAP : Days after plant")+
+  theme(legend.position = "none",
+        axis.text.x = element_text(size=26),
+        axis.text.y = element_text(size = 26),
+        axis.title = element_text(size = 32),
+        plot.title = element_text(size = 28),
+        plot.subtitle = element_text(size = 24), 
+        panel.grid.major.y = element_line(color = "darkgrey"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.caption = element_text(hjust = 0, size = 20, color = "grey25"))
 
 
 
